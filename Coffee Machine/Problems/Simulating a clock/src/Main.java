720fe6c69cb0d4d5302925586f641f1d11eac66c
Ed Maphis <emaphis85@gmail.com>
2020-08-07 21:12:37 -0400

    Enums

---
+++ Coffee Machine/Problems/Simulating a clock/src/Main.java
@@ -0,0 +1,23 @@
+class Clock {
+
+    int hours = 12;
+    int minutes = 0;
+
+    void next() {
+        // implement me
+        if (minutes == 59) {
+            minutes = 0;
+            nextHour();
+        } else {
+            minutes++;
+        }
+    }
+
+    void nextHour() {
+        if (hours == 12) {
+            hours = 1;
+        } else {
+            hours++;
+        }
+    }
+}
